0b68aa55cfe6fcca55925adeb2588f4a60edb72d
rkoschel <rudolf@koschel.org>
2021-11-18 22:54:21 +0100

    install node modules

--- pi-setup/setup.sh
+++ pi-setup/setup.sh
@@ -1,4 +1,9 @@
 
+## install node modules
+cd /home/pi/Apps/gwfjt/
+npm install
+cd -
+
 # setup and start gwfjt service
 sudo cp /home/pi/Apps/gwfjt/pi-setup/gwfjt.service /lib/systemd/system
 sudo chmod 644 /lib/systemd/system/gwfjt.service
