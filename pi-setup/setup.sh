99a91ebdf82e9a9490ebeda34222bc8eaa4bb170
rkoschel <rudolf@koschel.org>
2021-11-18 22:21:24 +0100

    pi setup files

---
+++ pi-setup/setup.sh
@@ -0,0 +1,7 @@
+
+# setup and start gwfjt service
+sudo cp /home/pi/Apps/gwfjt/pi-setup/gwfjt.service /lib/systemd/system
+sudo chmod 644 /lib/systemd/system/gwfjt.service
+sudo systemctl daemon-reload
+sudo systemctl enable gwfjt.service
+sudo systemctl start gwfjt.service
